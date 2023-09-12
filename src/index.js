24509cc7dc642ba4de3e73b18dd121344907f9b6
Rodrigo Gama <rodrigongama.adv@gmail.com>
2023-09-12 15:29:00 -0300

    feat: adding cors middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 require('express-async-errors');
 
+const cors = require('./app/middlewares/cors');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors);
 app.use(routes);
 app.use((error, request, response, next) => {
   console.log(error);
