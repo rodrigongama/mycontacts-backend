9b43749699d4823c1c00f41f544138d677f643ad
Rodrigo Gama <rodrigongama.adv@gmail.com>
2023-09-12 15:29:55 -0300

    refactor: error handler middleware

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('express-async-errors');
 
 const cors = require('./app/middlewares/cors');
+const errorHandler = require('./app/middlewares/errorHandler');
 const routes = require('./routes');
 
 const app = express();
@@ -9,9 +10,6 @@ const app = express();
 app.use(express.json());
 app.use(cors);
 app.use(routes);
-app.use((error, request, response, next) => {
-  console.log(error);
-  response.sendStatus(500);
-});
+app.use(errorHandler);
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000'));
