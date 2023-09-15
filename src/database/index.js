9f378e37dec0e4529b00129c76394e2c459cdb01
Rodrigo Gama <rodrigongama.adv@gmail.com>
2023-09-14 21:09:43 -0300

    refactor: pg client and add dotenv

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,25 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const { Client } = require('pg');
+require('dotenv').config();
 
-const client = new Client(process.env.DATABASE_URL);
+const { DATABASE_URL, NODE_ENV } = process.env;
+const sslConfig = NODE_ENV === 'production' ? { rejectUnauthorized: true } : false;
 
-client.connect();
+const client = new Client({
+  connectionString: DATABASE_URL,
+  ssl: sslConfig,
+});
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log('Connected to the database');
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
+}
+
+connectToDatabase();
 
 exports.query = async (query, values) => {
   const { rows } = await client.query(query, values);
